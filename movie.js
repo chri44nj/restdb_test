3a79fef797656a24dfa0baf9f45cd7735956e552
katbang <katr164m@stud.kea.dk>
2023-02-17 09:32:04 +0100

    produktside

--- movie.js
+++ movie.js
@@ -6,10 +6,19 @@ fetch("https://web2film-9d1b.restdb.io/rest/movies/63ea066baa86075000051761", {
 })
   .then((e) => e.json())
   .then(showMovie);
+
 function showMovie(movie) {
   console.log(movie);
+  document.querySelector(".description").textContent = movie.description;
   document.querySelector(".genre").textContent = movie.genre;
-  document.querySelector(".year").textContent = movie.year;
+  document.querySelector(".genre2").textContent = `Genre: ${movie.genre}`;
+  document.querySelector(".year").textContent = `Year: ${movie.year}`;
+  document.querySelector(".length").textContent = `Length: ${movie.length}m.`;
   document.querySelector(".title").textContent = movie.title;
-  document.querySelector(".poster").src = `img/${movie.poster}.webp`;
+  document.querySelector(".title2").textContent = movie.title;
+  document.querySelector(".poster").src = `img/${movie.poster}`;
+  document.querySelector(
+    ".director"
+  ).textContent = `Director: ${movie.director}`;
+  document.querySelector(".budget").textContent = `Budget: $${movie.budget}`;
 }
