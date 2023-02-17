9d010ed77c2a255049fdcba5fd4e1557d3937bb7
katbang <katr164m@stud.kea.dk>
2023-02-17 09:48:49 +0100

    kodet i blinde - produktside

--- movie.js
+++ movie.js
@@ -9,16 +9,32 @@ fetch("https://web2film-9d1b.restdb.io/rest/movies/63ea066baa86075000051761", {
 
 function showMovie(movie) {
   console.log(movie);
-  document.querySelector(".description").textContent = movie.description;
   document.querySelector(".genre").textContent = movie.genre;
+  document.querySelector(".title").textContent = movie.title;
+  document.querySelector(".title2").textContent = movie.title;
+  document.querySelector(".rating").textContent = movie.rating;
+  document.querySelector(".description").textContent = movie.description;
   document.querySelector(".genre2").textContent = `Genre: ${movie.genre}`;
   document.querySelector(".year").textContent = `Year: ${movie.year}`;
   document.querySelector(".length").textContent = `Length: ${movie.length}m.`;
-  document.querySelector(".title").textContent = movie.title;
-  document.querySelector(".title2").textContent = movie.title;
   document.querySelector(".poster").src = `img/${movie.poster}`;
   document.querySelector(
     ".director"
   ).textContent = `Director: ${movie.director}`;
   document.querySelector(".budget").textContent = `Budget: $${movie.budget}`;
+
+  if (movie.oscarNominated) {
+    copy.querySelector(".oscarNominated").textContent =
+      movie.oscarNominated + " Oscar nominations";
+  } else {
+    copy.querySelector(".oscarNominated").classList.add("hide");
+    copy.querySelector(".oscarWinner").classList.add("hide");
+  }
+
+  if (movie.oscarWinner) {
+    copy.querySelector(".oscarWinner").textContent =
+      movie.oscarWinner + " Oscar wins";
+  } else {
+    copy.querySelector(".oscarWinner").classList.add("hide");
+  }
 }
