9d9f8e83d06fc635bc0205841489a8e6c7dd50eb
katbang <katr164m@stud.kea.dk>
2023-02-18 08:41:37 +0100

    fetch virker stadig ikke

--- category.js
+++ category.js
@@ -1,12 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const movieGenre = urlParams.get("genre");
 
-fetch(`https://web2film-9d1b.restdb.io/rest/movies?q={"genre":"${movieGenre}"}`, {
-  method: "get",
-  headers: {
-    "x-apikey": "63ea0303478852088da68144",
-  },
-})
+fetch(
+  `https://web2film-9d1b.restdb.io/rest/movies?q={"genre":"${movieGenre}"}`,
+  {
+    method: "get",
+    headers: {
+      "x-apikey": "63ea0303478852088da68144",
+    },
+  }
+)
   .then((e) => e.json())
   .then(showCategories);
 
@@ -19,7 +22,7 @@ function showCategory(category) {
   const template = document.querySelector("#categoryTemplate").content;
   const copy = template.cloneNode(true);
   copy.querySelector("img").src = `img/${category.poster}`;
-  copy.querySelector("a").href = "movie.html?" + "id=" + category.id;
-  copy.querySelector(".genreName").textContent = category.genre;
+  copy.querySelector("a").href = "movie.html?id=" + category.id;
+  copy.querySelector(".genreNameCategories").textContent = category.genre;
   document.querySelector(".categoriesGrid").append(copy);
 }
