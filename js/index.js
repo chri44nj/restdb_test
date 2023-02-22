ebd1a8370034961683aec506a8a66e09c71b2ab1
Valentin <Chris_valentin@hotmail.com>
2023-02-22 09:29:08 +0100

    index lidt bedre

--- js/index.js
+++ js/index.js
@@ -14,42 +14,37 @@ function generateRandomNumber() {
 function showMovies(movies) {
   console.log(movies);
   let rndNum = generateRandomNumber();
-  document.querySelector(".movieSuggestionTitle").textContent =
-    movies[rndNum].title;
-  document.querySelector(
-    ".movieSuggestionPoster"
-  ).src = `img/${movies[rndNum].poster}`;
+  document.querySelector(".movieSuggestionTitle").textContent = movies[rndNum].title;
+  document.querySelector(".movieSuggestionPoster").src = `img/${movies[rndNum].poster}`;
   if (movies[rndNum].streaming.includes("Netflix")) {
     document.querySelector(".streaming").href = `https://www.netflix.com/dk/`;
-    document.querySelector(".streaming").textContent = `Go to Netflix`;
+    document.querySelector(".streaming").textContent = `Netflix`;
   } else {
     document.querySelector(".streaming").classList.add("hide");
   }
 
   if (movies[rndNum].streaming.includes("HBO Max")) {
     document.querySelector(".streaming2").href = `https://www.hbomax.com/dk/da`;
-    document.querySelector(".streaming2").textContent = `Go to HBO max`;
+    document.querySelector(".streaming2").textContent = `HBO max`;
   } else {
     document.querySelector(".streaming2").classList.add("hide");
   }
 
   if (movies[rndNum].streaming.includes("Disney+")) {
-    document.querySelector(
-      ".streaming3"
-    ).href = `https://www.disneyplus.com/da-dk/`;
-    document.querySelector(".streaming3").textContent = `Go to Disney+`;
+    document.querySelector(".streaming3").href = `https://www.disneyplus.com/da-dk/`;
+    document.querySelector(".streaming3").textContent = `Disney+`;
   } else {
     document.querySelector(".streaming3").classList.add("hide");
   }
   if (movies[rndNum].streaming.includes("Viaplay")) {
     document.querySelector(".streaming4").href = `https://viaplay.dk/dk-da/`;
-    document.querySelector(".streaming4").textContent = `Go to Viaplay`;
+    document.querySelector(".streaming4").textContent = `Viaplay`;
   } else {
     document.querySelector(".streaming4").classList.add("hide");
   }
   if (movies[rndNum].streaming.includes("Amazon Prime")) {
     document.querySelector(".streaming5").href = `https://www.primevideo.com/`;
-    document.querySelector(".streaming5").textContent = `Go to Amazon Prime`;
+    document.querySelector(".streaming5").textContent = `Amazon Prime`;
   } else {
     document.querySelector(".streaming5").classList.add("hide");
   }
